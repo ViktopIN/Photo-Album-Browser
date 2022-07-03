ea1c85ef804ceeb96036186993a414889152bdf1
Viktor Pinyagin <viktopiniaginya@gmail.com>
2022-07-03 21:22:19 +0500

    navigationbar setup in main collectionViewController

---
+++ Photo Album Browser/Entity/Strings.swift
@@ -0,0 +1,10 @@
+//
+//  Strings.swift
+//  Photo Album Browser
+//
+//  Created by t h a . m a m e rozz on 03.07.22.
+//
+
+enum Strings {
+    static let titleOfMainCollectionViewController = "Альбомы"
+}
--- Photo Album Browser/MainCollectionViewController/CollectionViewController.swift
+++ Photo Album Browser/MainCollectionViewController/CollectionViewController.swift
@@ -10,8 +10,37 @@ import UIKit
 private let reuseIdentifier = "Cell"
 
 class CollectionViewController: UICollectionViewController {
-
+//    MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        hierarchySetup()
+        layoutSetup()
+        viewSetup()
+    }
+//    MARK: - Settings
+    private func hierarchySetup() {
+        
+    }
+    
+    private func layoutSetup() {
+        
+    }
+    
+    private func viewSetup() {
+        view.backgroundColor = .systemBackground
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.largeTitleDisplayMode = .always
+        title = Strings.titleOfMainCollectionViewController
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus"),
+                                                           style: .plain,
+                                                           target: self,
+                                                           action: #selector(plusButtonAction))
+    }
+//    MARK: - Methods
+    @objc func plusButtonAction() {
+        print("Plus button tapped!")
     }
 }
