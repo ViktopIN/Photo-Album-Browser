7e1b16aa6de4e542a2572ffed6574bce6f75ae11
Viktor Pinyagin <viktopiniaginya@gmail.com>
2022-07-08 22:59:54 +0500

    setup entity

---
+++ Photo Album Browser/Entity/Album.swift
@@ -0,0 +1,14 @@
+//
+//  Album.swift
+//  Photo Album Browser
+//
+//  Created by t h a . m a m e rozz on 08.07.22.
+//
+import Foundation
+import UIKit.UIImage
+
+struct Album {
+    let image: UIImage
+    let name: String
+    let countItems: String
+}
